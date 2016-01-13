630a83fd4d2453c3f23b264d0c8b3670b61567e0
apple <apple@99.local>
2016-01-13 13:13:20 +0800

    Initial Commit

---
+++ GoldCatTests/GoldCatTests.m
@@ -0,0 +1,39 @@
+//
+//  GoldCatTests.m
+//  GoldCatTests
+//
+//  Created by apple on 16/1/13.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GoldCatTests : XCTestCase
+
+@end
+
+@implementation GoldCatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
